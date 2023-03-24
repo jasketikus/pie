a0fad932327c984d7a544a48207c0103178e3bd5
Jasketikus <kvfromcv@gmail.com>
2023-03-24 23:24:03 +0200

    added models of profile, characteristic and rating

--- pieproject/pieapp/models.py
+++ pieproject/pieapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    birth_date = models.DateField(null=True, blank=True)
+
+class Characteristic(models.Model):
+    name = models.CharField(max_length=100)
+
+class Rating(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    characteristic = models.ForeignKey(Characteristic, on_delete=models.CASCADE)
+    rating = models.IntegerField()
