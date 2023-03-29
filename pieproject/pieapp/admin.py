d1d63ea1f53aec4bb7ade777a7c18feffbf6e25b
Jasketikus <kvfromcv@gmail.com>
2023-03-29 21:52:11 +0300

    added characteristic model to admin panel

--- pieproject/pieapp/admin.py
+++ pieproject/pieapp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+class CharacteristicAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+
+
+admin.site.register(Characteristic, CharacteristicAdmin)
+
+admin.site.site_title = 'Administration panel'
+admin.site.site_header = 'Administration'
