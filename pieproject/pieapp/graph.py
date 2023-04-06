3776448d2a749e8dd4f826f62cb59b4b2ed5fe0f
Jasketikus <kvfromcv@gmail.com>
2023-04-06 21:05:26 +0300

    now graph is pie. edited styles

--- pieproject/pieapp/graph.py
+++ pieproject/pieapp/graph.py
@@ -1,26 +1,31 @@
-from bokeh.models import ColumnDataSource
-from bokeh.palettes import Bright6
+from math import pi
+
+import pandas as pd
+
+from bokeh.palettes import cividis
 from bokeh.plotting import figure, show
-from bokeh.transform import factor_cmap
+from bokeh.transform import cumsum
 from bokeh.embed import file_html
 from bokeh.resources import CDN
 
 
 def create_graph(**kwargs):
-    fruits = list(kwargs.keys())
-    counts = list(kwargs.values())
 
-    source = ColumnDataSource(data=dict(fruits=fruits, counts=counts))
+    x = kwargs
 
-    p = figure(x_range=fruits, height=350, toolbar_location=None, title="Characteristics")
+    data = pd.Series(x).reset_index(name='value').rename(columns={'index': 'country'})
+    data['angle'] = data['value']/data['value'].sum() * 2*pi
+    data['color'] = cividis(len(x))
 
-    p.vbar(x='fruits', top='counts', width=0.9, source=source, legend_field="fruits",
-        line_color='white', fill_color=factor_cmap('fruits', palette=Bright6, factors=fruits))
+    p = figure(height=400, title="Characteristics", toolbar_location=None,
+            tools="hover", tooltips="@country: @value", x_range=(-0.5, 1.0))
 
-    p.xgrid.grid_line_color = None
-    p.y_range.start = 0
-    p.y_range.end = 9
-    p.legend.orientation = "horizontal"
-    p.legend.location = "top_center"
+    p.wedge(x=0, y=1, radius=0.4,
+            start_angle=cumsum('angle', include_zero=True), end_angle=cumsum('angle'),
+            line_color="white", fill_color='color', legend_field='country', source=data)
 
+    p.axis.axis_label = None
+    p.axis.visible = False
+    p.grid.grid_line_color = None
+  
     return file_html(p, CDN, "graph")
